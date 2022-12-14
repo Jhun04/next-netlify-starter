ba0ddd3d5e3d4b4197a538fa0db160c6a752f344
Jhun04 <120516253+Jhun04@users.noreply.github.com>
2022-12-14 04:09:50 +0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
-}
+- export default function Header({ title }) {
+- return <h1 className="title">{welcome to my app}</h1>
++ export default function header ({title})
++ return <h1 classname="title">{title}</h1>
